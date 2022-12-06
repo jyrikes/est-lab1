f15e6822b947f49f507fc9ff45a7f84921fc8657
jyrikes <59875426+jyrikes@users.noreply.github.com>
2022-12-06 20:30:46 +0000

    primeiras estruturas

--- ListaArray.java
+++ ListaArray.java
@@ -1,26 +1,66 @@
 public class ListaArray implements EstruturaDeDados{
+    private int[] elementos ;
+    private int tamanho;
+    private int index;
+    private int posBusca;
+
+    public ListaArray(int tamanho) {
+        this.tamanho = tamanho;
+        this.elementos = new int[tamanho];
+        this.index = 0;
+        this.posBusca = 0;
+    }
 
     @Override
     public boolean insert(int chave) {
-        // TODO Auto-generated method stub
-        return false;
+
+        if(this.index < this.tamanho){
+            this.elementos[index] = chave;
+            this.index = index + 1;
+            return true;
+        }
+        else{
+            return false;
+        }   
+        
     }
 
     @Override
     public boolean delete(int chave) {
-        // TODO Auto-generated method stub
-        return false;
+        if(this.search(chave)){
+            
+            for(int i = this.tamanho-1 ; i > 0; i--){
+                    
+                    this.elementos[i-1] = this.elementos[i];
+                
+            }
+            this.index --;
+            return true;
+        }
+        else{
+
+
+            return false;
+
+        }
+        
+        
     }
 
     @Override
     public boolean search(int chave) {
-        // TODO Auto-generated method stub
+        for(int i = 0; i < this.index; i++ ){
+            if(chave == this.elementos[i]){
+                this.posBusca = i;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int minimum() {
-        // TODO Auto-generated method stub
+        
         return 0;
     }
 
@@ -41,8 +81,18 @@ public class ListaArray implements EstruturaDeDados{
         // TODO Auto-generated method stub
         return 0;
     }
+    
 
     public static void main(String[] args) {
-        ListaArray r = new ListaArray();
+        ListaArray r = new ListaArray(2);
+        boolean a = r.insert(10);
+        boolean b = r.insert(20);
+        boolean c =r.delete(20);
+        
+        //System.out.println(r.search(20));
+        //System.out.println(b);
+        System.out.println(r.elementos[0]);
     }
+   
+
 }
